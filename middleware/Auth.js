eaf8427e924e8f489cfe6c89afec27f883b7797a
oluwamayowaf <e_fadairo@yahoo.com>
2019-11-12 00:25:50 +0100

    feature(Registration): Updated the authorization middleware [Ends #009]

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -4,9 +4,7 @@ const db = require('../db/index');
 
 module.exports = {
   async validateToken(req, res, next) {
-    const authorizationHeader = req.headers.authorization;
-    let result = {};
-    if (authorizationHeader) {
+    if (req.headers.authorization) {
       const token = req.headers.authorization.split(' ')[1];// Bearer <token>
       const options = {
         expiresIn: '7d',
@@ -14,7 +12,6 @@ module.exports = {
       };
       try {
         const decoded = await jwt.verify(token, process.env.JWT_SECRET, options);
-
         const text = 'SELECT * FROM users WHERE id = $1';
         const { rows } = await db.query(text, [decoded.userId]);
         if (!rows[0]) {
@@ -25,21 +22,15 @@ module.exports = {
         }
         // Decoded token to be passed back to the request object
         const User = rows[0];
-
         req.user = { id: User.id };
         req.role = User.role;
-        req.User = User;
         next();
       } catch (err) {
         throw new Error(err);
       }
-    } else {
-      result = {
-        error: 'Authorization Token not found',
-        status: 401,
-      };
-      return res.status(401).send(result);
-    }
+    } return res.status(401).json({
+      error: 'Authorization Token not found',
+      status: 401,
+    });
   },
-
 };
