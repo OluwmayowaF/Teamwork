26bd8dddeb26b71dc849d34989d9a596a5dd8b76
oluwamayowaf <e_fadairo@yahoo.com>
2019-11-11 17:50:17 +0100

    chore(postgres):setup progres for database activities and setup conecttion file \n [Start #004]

---
+++ db/index.js
@@ -0,0 +1,32 @@
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+const environment = process.env.NODE_ENV;
+const stage = require('../config')[environment];
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: stage.DBHost,
+});
+
+module.exports = {
+  /**
+     *  DB Query
+     *  @param {object} req
+     *  @param {object} res
+     *  @returns {object} object
+     */
+  query(text, params) {
+    return new Promise((resolve, reject) => {
+      pool.query(text, params)
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  },
+
+};
