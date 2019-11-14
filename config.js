945bb1b531dafc930907ecc745d107e245925977
oluwamayowaf <e_fadairo@yahoo.com>
2019-11-14 09:45:53 +0100

    Enable SSL mode for db postgres

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ module.exports = {
     DBHost: process.env.DB_TEST_CONN_URL,
   },
   production: {
-    port: process.env.PORT || 3000,
-    DBHost: process.env.DB_LOCAL_CONN_URL,
+    port: process.env.PORT || 5000,
+    DBHost: process.env.DB_CONN_URL,
   },
 
 };
