775098253b5dc0ad4eb7187dfc11bf37c08fb6a6
oluwamayowaf <e_fadairo@yahoo.com>
2019-11-14 06:43:40 +0100

    Updates to the Gifs controller

--- controller/gifs.js
+++ controller/gifs.js
@@ -4,42 +4,28 @@ const Helper = require('../controller/helper');
 // const cloudinary = require('cloudinary').v2;
 const db = require('../db/index');
 
-
-
 module.exports = {
   async createGif(req, res) {
     if (!req.files || req.files.image.mimetype !== 'image/gif') {
-      return res.status(400).json({
-        status: 'error',
-        error: 'Kindly upload a gif to proceed',
-      });
+      return res.status(400).json({ status: 'error', error: 'Kindly upload a gif to proceed' });
     }
     if (!req.body.title) {
-      return res.status(400).json({
-        status: 'error',
-        error: 'Enter a title for your gif post',
-      });
+      return res.status(400).json({ status: 'error', error: 'Enter a title for your gif post' });
     }
-
-
     const image = await Helper.uploadToCloudinary(req.files.image);
 
-    const addUrl = `INSERT INTO 
-          gifs(ownerId, title, imageUrl)
-          values($1, $2, $3) returning *`;
+    const addUrl = 'INSERT INTO gifs(ownerId, title, imageUrl) values($1, $2, $3) returning *';
     const values = [req.user.id, req.body.title, image.url];
-
     try {
       const { rows } = await db.query(addUrl, values);
       return res.status(201).json({
         status: 'success',
         data: {
-          message: 'GIF image successfully posted',
           gifId: rows[0].id,
+          message: 'GIF image successfully posted',
           createdOn: rows[0].created_on,
           title: rows[0].title,
-          article: rows[0].imageurl,
-          gif: rows[0],
+          imageUrl: rows[0].imageurl,
         },
       });
     } catch (error) {
@@ -58,10 +44,7 @@ module.exports = {
     try {
       const { rows } = await db.query(findgif, values);
       if (!rows[0]) {
-        return res.status(404).json({
-          status: 'error',
-          error: 'Gif was not found!!',
-        });
+        return res.status(404).json({ status: 'error', error: 'Gif was not found!!' });
       }
       const url = rows[0].imageurl;
       const publicId = path.basename(url, '.gif');
@@ -80,33 +63,19 @@ module.exports = {
   },
   async addComment(req, res) {
     if (!req.body.comment) {
-      return res.status(400).json({
-        status: 'error',
-        error: 'Your comment must have some content',
-      });
+      return res.status(400).json({ status: 'error', error: 'Your comment must have some content' });
     }
     // Fetch Gif
-    const findgif = `SELECT * FROM
-    gifs WHERE id = $1`;
-
-    const addComment = `INSERT INTO 
-    gifs_comments(ownerId, gifId, comment) 
-    values($1, $2, $3) returning *`;
-
-    const values = [
-      req.user.id,
-      req.params.gifId,
-      req.body.comment,
-    ];
+    const findgif = 'SELECT * FROM gifs WHERE id = $1';
+    const addComment = 'INSERT INTO gifs_comments(ownerId, gifId, comment) values($1, $2, $3) returning *';
+    const values = [req.user.id, req.params.gifId, req.body.comment];
 
     try {
       const gif = await db.query(findgif, [req.params.gifId]);
       if (!gif.rows[0]) {
-        return res.status(404).json({
-          status: 'error',
-          error: 'Gif was not found!',
-        });
-      } const comment = await db.query(addComment, values);
+        return res.status(404).json({ status: 'error', error: 'Gif was not found!' });
+      }
+      const comment = await db.query(addComment, values);
       return res.status(201).json({
         status: 'success',
         data: {
@@ -121,16 +90,12 @@ module.exports = {
       return res.status(500).send(error);
     }
   },
+
   async getGif(req, res) {
     let comment = {};
-    const findGif = `SELECT *
-    FROM gifs
-    WHERE id = $1`;
-    const findComment = `SELECT 
-    id as commentId, comment, ownerId as authorId
-    FROM
-    gifs_comments WHERE gifId= $1`;
-
+    const findGif = 'SELECT * FROM gifs WHERE id = $1';
+    const findComment = `SELECT  id as commentId, comment, ownerId as authorId
+    FROM gifs_comments WHERE gifId= $1`;
     try {
       const { rows } = await db.query(findGif, [req.params.gifId]);
       if (!rows[0]) {
@@ -139,9 +104,7 @@ module.exports = {
       const comments = await db.query(findComment, [req.params.gifId]);
       if (!comments.rows[0]) {
         comment = 'No Comments have been added to this gif';
-      } else {
-        comment = comments.rows;
-      }
+      } else { comment = comments.rows; }
       return res.status(200).json({
         status: 'success',
         data: {
