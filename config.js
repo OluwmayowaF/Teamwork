3e80cd0a50df4b2bd3b0d73f257ed98023e67c95
oluwamayowaf <e_fadairo@yahoo.com>
2019-11-14 09:05:13 +0100

    Bug(FileSize)Updates to Config file.js

--- config.js
+++ config.js
@@ -7,5 +7,9 @@ module.exports = {
     port: process.env.PORT || 8001,
     DBHost: process.env.DB_TEST_CONN_URL,
   },
+  port: {
+    port: process.env.PORT || 3000,
+    DBHost: process.env.DB_LOCAL_CONN_URL,
+  },
 
 };
