c915c3a7714f715bbe1164aa8fb67aa4ceb6f5fa
oluwamayowaf <e_fadairo@yahoo.com>
2019-11-22 19:48:31 +0100

    BugFix(Authorization)Allow API recive token in request header Cors

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/', (_req, res) => {
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,Authorization');
   next();
 });
 app.use('/api/v1', routes(router));
