29d6b7f862855434cbd7d00cc687e364ad678429
oluwamayowaf <e_fadairo@yahoo.com>
2019-11-28 17:50:59 +0100

    Bug() Cors Update

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.get('/', (_req, res) => {
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   next();
 });
 app.use('/api/v1', routes(router));
